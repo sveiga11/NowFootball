d9cbccc6a341ad4b796cd3d1f1a6cb2077396cb9
Samuel Veiga <sveiga@elpuig.xeill.net>
2022-12-10 23:33:13 +0100

    Libreria Graficos

--- app/src/main/java/com/example/nowfootball/EstadisticasSeleccion1.java
+++ app/src/main/java/com/example/nowfootball/EstadisticasSeleccion1.java
@@ -1,64 +1,42 @@
 package com.example.nowfootball;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EstadisticasSeleccion1#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.nowfootball.databinding.FragmentEstadisticasSeleccion1Binding;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
+
 public class EstadisticasSeleccion1 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private FragmentEstadisticasSeleccion1Binding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EstadisticasSeleccion1() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EstadisticasSeleccion1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EstadisticasSeleccion1 newInstance(String param1, String param2) {
-        EstadisticasSeleccion1 fragment = new EstadisticasSeleccion1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_estadisticas_seleccion1, container, false);
+        return (binding = FragmentEstadisticasSeleccion1Binding.inflate(inflater, container, false)).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        PieChart mPieChart = (PieChart) view.findViewById(R.id.graficoPartidos);
+
+        mPieChart.addPieSlice(new PieModel("Empatados", 2, Color.parseColor("#FED70E")));
+        mPieChart.addPieSlice(new PieModel("Ganados", 1 , Color.parseColor("#00FF19")));
+        mPieChart.addPieSlice(new PieModel("Perdidos", 1, Color.parseColor("#FF1605")));
+
+        mPieChart.startAnimation();
     }
 }
